5520363252054f5bafef8c870e828af11f895993
jEuEaEn <jarm1981avrm@gmail.com>
2025-12-09 17:56:10 -0500

    Co-authored-by: nsxd0908 <nsxd0908@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("npu").addEventListener("click", () => {
+            document.getElementById("npu").contentEditable = "true"
+            document.getElementById("npu").textContent = ""
+        })
