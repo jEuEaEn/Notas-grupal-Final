802083cd4c687c34ee1888978f8ff2c3d3fd6f7f
Tu Nombre <juandiegorojasmolinares2012@gmail.com>
2025-12-10 19:38:27 -0500

    PArte del lago izquierdo alumbrado

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-document.getElementById("npu").addEventListener("click", () => {
-            document.getElementById("npu").contentEditable = "true"
-            document.getElementById("npu").textContent = ""
-        })
+const botones = document.querySelectorAll(".sidebar-btn");
+
+botones.forEach(btn => {
+    btn.addEventListener("click", () => {
+
+        // Quita activo de todos
+        botones.forEach(b => b.classList.remove("activo"));
+
+        // Activa el que tocaste
+        btn.classList.add("activo");
+    });
+});
